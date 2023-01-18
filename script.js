2a4e72683deb6b0ff6568fc2792c3d78355ff8dc
Tama-Shil <jucse28.342@gmail.com>
2023-01-18 12:10:41 +0600

    add alert

--- script.js
+++ script.js
@@ -3,16 +3,14 @@ let count = 0;
 function setCount() {
   document.getElementById("output").innerText = count;
 }
-
 function setInitialCount() {
   setCount();
 }
-
 function increment() {
   count++;
   setCount();
   if (count > 10) {
-    alert("Stoppppppppppp");
+    alert("You typed more than 10 times");
   }
 }
 
